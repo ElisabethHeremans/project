5fc54f9378039917155df356c4bfd2bb6a8b3b67
MoranGybels <mooke_moran@hotmail.com>
2016-04-21 15:29:14 +0200

    Task

--- OGP1516-Hillbillies/src/hillbillies/model/Task.java
+++ OGP1516-Hillbillies/src/hillbillies/model/Task.java
@@ -8,16 +8,21 @@ import be.kuleuven.cs.som.annotate.*;
  * @invar Each task can have its name as name . | canHaveAsName(this.getName())
  * @invar The priority of each task must be a valid priority for any task. |
  *        isValidPriority(getPriority())
+ * @invar The activities of each task must be a valid activities for any task. |
+ *        isValidActivities(getActivities())
  */
 
 public class Task {
 	/**
-	 * Initialize this new task with given name and the given priority.
+	 * Initialize this new task with given name, the given priority and the
+	 * given activities.
 	 * 
 	 * @param name
 	 *            The name for this new task.
 	 * @param priority
 	 *            The priority for this new task.
+	 * @param activities
+	 *            The activities for this new task.
 	 * @post If the given name is a valid name for any task, the name of this
 	 *       new task is equal to the given name. Otherwise, the name of this
 	 *       new task is equal to "task". | if (isValidName(name)) | then
@@ -27,15 +32,18 @@ public class Task {
 	 *       Otherwise, the priority of this new task is equal to 0. | if
 	 *       (isValidPriority(priority)) | then new.getPriority() == priority |
 	 *       else new.getPriority() == 0
+	 * @effect The activities of this new task is set to the given activities. |
+	 *         this.setActivities(activities)
 	 */
 
-	public Task(String name, int priority, List<List> activities) {
+	public Task(String name, int priority, Statement activities) throws IllegalArgumentException {
 		if (!canHaveAsName(name))
 			name = "task";
 		this.name = name;
 		if (!isValidPriority(priority))
 			priority = 0;
 		setPriority(priority);
+		this.setActivities(activities);
 	}
 
 	/**
@@ -116,5 +124,48 @@ public class Task {
 	 * Variable registering the priority of this task.
 	 */
 	private int priority;
+
+	/**
+	 * Return the activities of this task.
+	 */
+	@Basic
+	@Raw
+	public Statement getActivities() {
+		return this.activities;
+	}
+
+	/**
+	 * Check whether the given activities is a valid activities for any task.
+	 * 
+	 * @param activities
+	 *            The activities to check.
+	 * @return | result == (activities != null)
+	 */
+	public static boolean isValidActivities(Statement activities) {
+		return (activities != null);
+	}
+
+	/**
+	 * Set the activities of this task to the given activities.
+	 * 
+	 * @param activities
+	 *            The new activities for this task.
+	 * @post The activities of this new task is equal to the given activities. |
+	 *       new.getActivities() == activities
+	 * @throws IllegalArgumentException
+	 *             The given activities is not a valid activities for any task.
+	 *             | ! isValidActivities(getActivities())
+	 */
+	@Raw
+	public void setActivities(Statement activities) throws IllegalArgumentException {
+		if (!isValidActivities(activities))
+			throw new IllegalArgumentException();
+		this.activities = activities;
+	}
+
+	/**
+	 * Variable registering the activities of this task.
+	 */
+	private Statement activities;
 
 }
