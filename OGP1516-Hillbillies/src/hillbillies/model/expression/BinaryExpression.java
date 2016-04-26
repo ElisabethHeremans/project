45f1a38553606ba4fa5200a06e26d5fbe8e749d1
ElisabethHeremans <elisabethrmh@outlook.com>
2016-04-27 00:59:05 +0200

    expr

--- OGP1516-Hillbillies/src/hillbillies/model/expression/BinaryExpression.java
+++ OGP1516-Hillbillies/src/hillbillies/model/expression/BinaryExpression.java
@@ -1,5 +1,34 @@
 package hillbillies.model.expression;
 
 public class BinaryExpression extends ComposedExpression {
-
+	
+	public BinaryExpression(Expression right, Expression left){
+		this.rightExp = right;
+		this.leftExp = left;
+				
+	}
+	
+	
+	public Expression getRightExpression(){
+		return rightExp;
+	}
+	public Expression getLeftExpression(){
+		return leftExp;
+	}
+	
+	private Expression rightExp;
+	private Expression leftExp;
+	
+	public isValidExpression(Expression expression){
+		if (this.leftExp instanceof PositionExpression)
+			return this.rightExp instanceof PositionExpression;
+		if (this.leftExp instanceof UnitExpression)
+			return this.rightExp instanceof UnitExpression;
+		if (this.leftExp instanceof BooleanExpression)
+			return this.rightExp instanceof BooleanExpression;
+		if (this.leftExp instanceof VariableExpression)
+			return this.rightExp instanceof VariableExpression;
+		//?
+					
+	}
 }
