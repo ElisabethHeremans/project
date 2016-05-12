ed4d633ad6e01d24699303c3776c36d5b99aefbd
MoranGybels <mooke_moran@hotmail.com>
2016-05-12 15:49:53 +0200

    Stream FriendExpression

--- OGP1516-Hillbillies/src/hillbillies/model/expression/FriendExpression.java
+++ OGP1516-Hillbillies/src/hillbillies/model/expression/FriendExpression.java
@@ -1,35 +1,50 @@
 package hillbillies.model.expression;
 
+import java.util.Comparator;
+import java.util.Optional;
+
 import hillbillies.model.ExecutionContext;
 import hillbillies.model.Unit;
 import hillbillies.model.Vector;
-import hillbillies.model.types.FriendType;
 
 public class FriendExpression extends UnitExpression{
 	
 	public FriendExpression(){
+		
+	}
+	
+	private Unit findFriendUnitStream(Unit unit){
+
+		final Comparator<Unit> comp = (u1, u2) -> Integer.compare( (int) Vector.getDistance(u1.getPosition(), unit.getPosition()), 
+				(int) Vector.getDistance(u2.getPosition(), unit.getPosition()));
+		
+		Optional<Unit> nearest = unit.getWorld().listAllUnits().stream()
+				.filter(n -> n.getFaction() == unit.getFaction())
+				.min(comp);
+		
+		return nearest.get();
 	}
 
-	private Unit findFriendUnit(Unit unit) {
-		//Unit unit = this.getStatement().getTask().getExecutingUnit();
-		assert (unit!=null);
-		Unit nearest = null;
-		double nearestDist = 0;
-		for (Unit other: unit.getWorld().listAllUnitsOfFaction(unit.getFaction())){
-			if (other !=unit){
-				double dist = Vector.getDistance(unit.getPosition(), other.getPosition());
-				if (nearest == null || nearestDist > dist){
-					nearestDist = dist;
-					nearest = other;
-				}
-			}
-		}
-		return nearest;
-	}
+//	private Unit findFriendUnit(Unit unit) {
+//		//Unit unit = this.getStatement().getTask().getExecutingUnit();
+//		assert (unit!=null);
+//		Unit nearest = null;
+//		double nearestDist = 0;
+//		for (Unit other: unit.getWorld().listAllUnitsOfFaction(unit.getFaction())){
+//			if (other !=unit){
+//				double dist = Vector.getDistance(unit.getPosition(), other.getPosition());
+//				if (nearest == null || nearestDist > dist){
+//					nearestDist = dist;
+//					nearest = other;
+//				}
+//			}
+//		}
+//		return nearest;
+//	}
 
 	@Override
 	public Unit evaluateExpression(ExecutionContext context) {
-		setValue(findFriendUnit(context.getExecutingUnit()));
+		setValue(findFriendUnitStream(context.getExecutingUnit()));
 		return getValue();
 	}
 }
